aff5e7b5f78ea6af5c3384646107b292e761f282
novalrnv <akuunoval@gmail.com>
2026-03-12 06:21:12 +0700

    form posts

---
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\DateTimePicker;
+
+class PostForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+                TextInput::make('title'),
+                TextInput::make('slug'),
+                Select::make('category_id')
+                    ->label('Category')
+                    ->options(\App\Models\Category::all()->pluck('name', 'id'))
+                    ->required(),
+                ColorPicker::make('color'),
+                // MarkdownEditor::make('content'),
+                RichEditor::make('content'),
+                FileUpload::make('Image')
+                    ->disk("public")
+                    ->directory("posts"),
+                TagsInput::make("tags"),
+                Checkbox::make("published"),
+                DateTimePicker::make("published_at"),
+            ]);
+    }
+}
