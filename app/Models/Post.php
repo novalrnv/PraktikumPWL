9100f34ebad8ae4340f456fb4fdee4ac21a41467
novalrnv <akuunoval@gmail.com>
2026-03-11 22:55:24 +0700

    relasi model post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,4 +23,8 @@ class Post extends Model
         'published' => 'boolean',
         'published_at' => 'date',
     ];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
