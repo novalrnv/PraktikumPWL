e6997d66d7fabf8f0820a6e1b00fcb5b7aed99b8
novalrnv <akuunoval@gmail.com>
2026-03-31 22:06:43 +0700

    Jobsheet 9

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\Products\Schemas;
 
 use Filament\Schemas\Schema;
 use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\IconEntry;
@@ -14,6 +16,82 @@ class ProductInfolist
     {
         return $schema
             ->components([
+                Tabs::make('Product Tabs')
+                    ->tabs([
+                        Tab::make('Product Details')
+                            ->icon('heroicon-m-information-circle')
+                            ->schema([
+                                TextEntry::make('name')
+                                    ->label('Product Name')
+                                    ->weight('bold')
+                                    ->color('primary'),
+
+                                TextEntry::make('id')
+                                    ->label('Product ID'),
+
+                                TextEntry::make('sku')
+                                    ->label('Product SKU')
+                                    ->badge()
+                                    ->color('success'),
+
+                                TextEntry::make('description')
+                                    ->label('Product Description'),
+
+                                TextEntry::make('created_at')
+                                    ->label('Product Creation Date')
+                                    ->date('d M Y')
+                                    ->color('info'),
+                            ]),
+                        Tab::make('Product Price and Stock')
+                            ->icon('heroicon-m-currency-dollar')
+                            ->schema([
+                                TextEntry::make('price')
+                                    ->label('Product Price')
+                                    ->weight('bold')
+                                    ->color('primary')
+                                    ->formatStateUsing(fn($state) => 'Rp ' . number_format($state, 0, ',', '.')),
+                                TextEntry::make('stock')
+                                    ->label('Product Stock')
+                                    ->weight('bold')
+                                    ->icon('heroicon-m-archive-box')
+                                    ->badge()
+                                    ->color(fn(string $state): string => match (true) {
+                                        $state >= 50 => 'success',
+                                        $state > 10 => 'info',
+                                        $state > 0 => 'warning',
+                                        default => 'danger',
+                                    }),
+                            ]),
+                        Tab::make('Image and Status')
+                            ->icon('heroicon-m-photo')
+                            ->schema([
+                                ImageEntry::make('image')
+                                    ->label('Product Image')
+                                    ->disk('public'),
+                                TextEntry::make('price')
+                                    ->label('Product Price')
+                                    ->weight('bold')
+                                    ->color('primary')
+                                    ->icon('heroicon-s-currency-dollar'),
+                                TextEntry::make('stock')
+                                    ->label('Product Stock')
+                                    ->weight('bold')
+                                    ->badge()
+                                    ->color(fn(string $state): string => match (true) {
+                                        $state >= 50 => 'success',
+                                        $state > 10 => 'info',
+                                        $state > 0 => 'warning',
+                                        default => 'danger',
+                                    }),
+                                IconEntry::make('is_active')
+                                    ->label('Is Active?')
+                                    ->boolean(),
+                                IconEntry::make('is_featured')
+                                    ->label('Is Featured?')
+                                    ->boolean(),
+                            ]),
+                    ])->columnSpanFull()
+                    ->vertical(),
                 Section::make('Product Info')
                     ->description('')
                     ->schema([
@@ -46,7 +124,14 @@ class ProductInfolist
                         TextEntry::make('stock')
                             ->label('Product Stock')
                             ->weight('bold')
-                            ->icon('heroicon-m-archive-box'),
+                            ->icon('heroicon-m-archive-box')
+                            ->badge()
+                            ->color(fn(string $state): string => match (true) {
+                                $state >= 50 => 'success',
+                                $state > 10 => 'info',
+                                $state > 0 => 'warning',
+                                default => 'danger',
+                            }),
                     ])
                     ->columnSpanFull(),
                 Section::make('Image and Status')
